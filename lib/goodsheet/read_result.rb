596718c97c723469aef99325afbc6af54ca417d0
Iwan Buetti <iwan.buetti@gmail.com>
2013-09-19 23:26:21 +0200

    result values by rows

--- lib/goodsheet/read_result.rb
+++ lib/goodsheet/read_result.rb
@@ -1,11 +1,11 @@
 module Goodsheet
   
   class ReadResult
-    attr_reader :values
+    attr_reader :vv, :errors
 
     def initialize(errors=ValidationErrors.new)
       @errors = errors
-      @values = {}
+      @vv = {}
     end
 
     def valid?
@@ -18,11 +18,30 @@ module Goodsheet
 
     def add(attribute, row, force_nil=nil)
       attribute = attribute.to_sym
-      (@values[attribute] ||= []) << (row.send(attribute) || force_nil)
+      (@vv[attribute] ||= []) << (row.send(attribute) || force_nil)
     end
 
-    def errors
-      @errors.array
+    def values(format=:columns)
+      values_size = @vv.values.first.size
+
+      case format
+      when :columns
+        @vv
+
+      when :rows_array
+        Array.new(values_size) do |i1|
+          Array.new(@vv.size) do |i2|
+            @vv[@vv.keys[i2]][i1]
+          end
+        end
+
+      when :rows_hash
+        Array.new(values_size) do |i1|
+          hh = {}
+          @vv.keys.each{|k| hh[k] = @vv[k][i1] }
+          hh
+        end
+      end
     end
   end
 end
