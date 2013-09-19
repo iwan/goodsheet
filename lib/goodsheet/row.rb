be58017e662817aa3e0eeb0bd00ca082d5f96e06
Iwan Buetti <iwan.buetti@gmail.com>
2013-09-19 16:27:45 +0200

    Added new way to define column_names

--- lib/goodsheet/row.rb
+++ lib/goodsheet/row.rb
@@ -30,37 +30,47 @@ module Goodsheet
     end
 
     # Define the position (or index) and the name of columns.
-    # There are available three mode to define them:
-    # using an hash index to name (like { 0 => :year, 2 => :day }) 
-    # or name to index (like { :year => 0, :day => 2 }) or using an array
+    # You have four ways to define them: 
+    # using an hash index-to-name (like { 0 => :year, 2 => :day }) 
+    # or its reversed version name-to-index (like { :year => 0, :day => 2 }), using an array
     # with the names at desired positions (like [:year, nil, :day]), put a nil
-    # at the position 
+    # at the position, or simply put the list of names.
     # The positions are 0-based.
-    def self.column_names(param)
+    def self.column_names(*attr)
+    # def self.column_names(param)
       @keys = {}
-      if param.is_a? Hash
-        if param.first[0].is_a? Integer
-          param.each do |idx, name|
-            self.keys[idx] = name
-            attr_accessor name
-          end
-        else
-          param.each do |name, idx|
-            self.keys[idx] = name
-            attr_accessor name
-          end
-        end
-      elsif param.is_a? Array
-        param.each_with_index do |name, idx|
+      raise ArgumentError, 'You have to pass at least one attribute' if attr.empty?
+      if attr[0].is_a? Array
+        attr[0].each_with_index do |name, idx|
           if name
             self.keys[idx] = name
             attr_accessor name        
           end
         end
+        
+      elsif attr[0].is_a? Hash
+        if attr[0].first[0].is_a? Integer
+          attr[0].each do |idx, name|
+            self.keys[idx] = name
+            attr_accessor name
+          end
+        else
+          attr[0].each do |name, idx|
+            self.keys[idx] = name
+            attr_accessor name
+          end
+        end
 
       else
-        raise "parameter non valid"
+        attr.each_with_index do |name, idx|
+          if name
+            name = name.to_s.gsub(" ", "_").to_sym unless name.is_a? Symbol
+            self.keys[idx] = name
+            attr_accessor name        
+          end
+        end
       end
+
     end
 
 
