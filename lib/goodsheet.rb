8c780215f980c20c743096be76cffbfc65722211
Iwan Buetti <iwan.buetti@gmail.com>
2013-09-24 15:41:28 +0200

    cleaning

--- lib/goodsheet.rb
+++ lib/goodsheet.rb
@@ -4,9 +4,9 @@ module Goodsheet
   autoload :Row,                'goodsheet/row'
   autoload :SheetNotFoundError, 'goodsheet/exceptions'
   autoload :ReadResult,         'goodsheet/read_result'
-  autoload :Row,                'goodsheet/row'
   autoload :Spreadsheet,        'goodsheet/spreadsheet'
   autoload :ValidationError,    'goodsheet/validation_error'
   autoload :ValidationErrors,   'goodsheet/validation_errors'
   autoload :Version,            'goodsheet/version'
 end
+
