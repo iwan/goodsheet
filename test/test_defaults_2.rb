ab600e38e4ed5fb48cd7db2fc84b070dfe588294
Iwan Buetti <iwan.buetti@gmail.com>
2013-10-29 14:36:54 +0100

    added functionality test of column_defaults macro when used before column_names macro

---
+++ test/test_defaults_2.rb
@@ -0,0 +1,130 @@
+require 'test/unit'
+require 'goodsheet'
+
+# test the functionality of column_defaults macro, when used before column_names macro
+class TestDefaults < Test::Unit::TestCase
+
+  def setup
+    filepath = File.dirname(__FILE__) + "/fixtures/for_defaults.xls"
+    @ss = Goodsheet::Spreadsheet.new(filepath, :skip => 1)
+  end
+
+
+  def test_values_correctness
+    res = @ss.read do
+      column_defaults 0 => "UNKNOWN", 1 => 0.0
+      column_names 0 => :name, 1 => :number
+      validates :name, :presence => true
+      validates :number, :numericality => true
+    end
+    assert(res.valid?)
+    assert_equal(["Joshua Baney", "Libby Meiers", "Dean Murdoch", "UNKNOWN", "Esmeralda Erben"], res.values[:name])
+    assert_equal([3.0, 2.0, 0.0, 8.0, 1.0], res.values[:number])
+  end
+
+
+  def test_invalid
+    res = @ss.read do
+      column_names 0 => :name, 1 => :number
+      validates :name, :presence => true
+      validates :number, :numericality => true
+    end
+    assert(res.invalid?)
+
+    res = @ss.read do
+      column_defaults 0 => "UNKNOWN"
+      column_names 0 => :name, 1 => :number
+      validates :name, :presence => true
+      validates :number, :numericality => true
+    end
+    assert(res.invalid?)
+
+    res = @ss.read do
+      column_defaults 1 => 0.0
+      column_names 0 => :name, 1 => :number
+      validates :name, :presence => true
+      validates :number, :numericality => true
+    end
+    assert(res.invalid?)
+  end
+
+
+
+  def test_force_nil_vs_defaults
+    res = @ss.read(:force_nil => 1.0) do
+      column_defaults 0 => "UNKNOWN", 1 => 10.0
+      column_names 0 => :name, 1 => :number
+      validates :name, :presence => true
+      validates :number, :numericality => true
+    end
+    assert(res.valid?)
+    assert_equal(["Joshua Baney", "Libby Meiers", "Dean Murdoch", "UNKNOWN", "Esmeralda Erben"], res.values[:name])
+    assert_equal([3.0, 2.0, 10.0, 8.0, 1.0], res.values[:number])
+  end
+
+
+
+
+
+
+  def test_not_existing_column
+    res = @ss.read do
+      column_defaults 0 => "UNKNOWN", 1 => 0.0
+      column_names 0 => :name, 1 => :number, 2 => :fake
+      validates :name, :presence => true
+      validates :number, :numericality => true
+    end
+    assert(res.valid?)
+    assert_equal([nil, nil, nil, nil, nil], res.values[:fake])
+
+    res = @ss.read do
+      column_defaults 0 => "UNKNOWN", 1 => 0.0
+      column_names 0 => :name, 1 => :number, 2 => :fake
+      validates :name, :presence => true
+      validates :number, :numericality => true
+      validates :fake, :presence => true
+    end
+    assert(res.invalid?)
+
+    res1 = @ss.read do
+      column_defaults 0 => "UNKNOWN", 1 => 0.0, 2 => "EMPTY"
+      column_names 0 => :name, 1 => :number, 2 => :fake
+      validates :name, :presence => true
+      validates :number, :numericality => true
+      validates :fake, :presence => true
+    end
+    assert(res1.valid?)
+  end
+
+
+
+  def test_column_defaults_formats
+    res_1 = @ss.read do
+      column_defaults ["UNKNOWN", 0.0]
+      column_names :name, :number
+      validates :name, :presence => true
+      validates :number, :numericality => true
+    end
+    assert(res_1.valid?)
+
+    res_2 = @ss.read do
+      column_defaults "UNKNOWN", 0.0
+      column_names :name, :number
+      validates :name, :presence => true
+      validates :number, :numericality => true
+    end
+    assert(res_2.valid?)
+    assert_equal(res_1.values, res_2.values)
+
+    res_3 = @ss.read do
+      column_defaults 1 => 0.0, 0 => "UNKNOWN"
+      column_names 0 => :name, 1 => :number
+      validates :name, :presence => true
+      validates :number, :numericality => true
+    end
+    assert(res_3.valid?)
+    assert_equal(res_1.values, res_3.values)
+  end
+
+
+end
